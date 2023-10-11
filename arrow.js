c02227c51af2d3603fed2412f8b25475a831bd4a
leemingoo <dwmapi@naver.com>
2023-10-11 12:11:21 +0900

    여러가지 응용

--- arrow.js
+++ arrow.js
@@ -1,12 +1,18 @@
-function outer() {
-  return "hello";
+function outerOne(a) {
+  function innerOne(b) {
+    console.log(b);
+  }
+  innerOne(a);
 }
 
-const outerTow = function() {
-  return "bye";
+function outerTwo(a) {
+  const innerTwo = b => console.log(b);
+  innerTwo(a);
 }
 
-const outerThree = () => "wow";
+console.log(outerOne("hello"));
+console.log(outerTwo("hello"));
 
-console.log(outer());
-console.log(outerTwo());
+
+// 콜백함수 내에서 익명함수를 사용시 재사용은 어떻게 해야하나
+//this가 보이면 console.log 를 찍는다 했는데 디버거로 확인이 안되는지
