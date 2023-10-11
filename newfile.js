9cf88adb2679680639eef3bf28db53b2a456cc90
leemingoo <dwmapi@naver.com>
2023-10-11 11:11:55 +0900

    test

--- newfile.js
+++ newfile.js
@@ -1,12 +1,14 @@
-function student(name, number) {
+function Student(name, number) {
   this.name = name;
   this.number = number;
 }
 
-let test = new student("이민구", "0");
-console.log(test);
-
-let studentList = {
-  name: 'hello',
-  number: 1
-};
+let list = [
+  "이민구",
+  "윤준현"
+];
+let studentArr = [];
+for (let i = 0; i < list.length; i++) {
+  studentArr[i] = new Student(list[i], i + 1);
+}
+console.log(studentArr);
